aed901dcf3063053832759e9dcc98c01619f31e4
anthony <nvhoanganh1909@gmail.com>
2020-04-10 14:57:45 +1000

    add to gcloud functions

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # Generate WORD document from template file using https://github.com/elapouya/python-docx-template
 
 ## Set up
-- clone the solution 
-- Inside the root folder, create new Python env: `python -m venv .venv`
-- In VScode, select the newly created Python env using `Python: Select Interpreter` option
-- Start new terminal session (using the newly created env)
-- Install dependencies: `pip install -r requirements.txt` 
-- modify the data in the `context` property
-- run `python main.py`
-- open the `output.docx` file in the output folder
+
+-   clone the solution
+-   Inside the root folder, create new Python env: `python -m venv .venv`
+-   In VScode, select the newly created Python env using `Python: Select Interpreter` option
+-   Start new terminal session (using the newly created env)
+-   Install dependencies: `pip install -r requirements.txt`
+-   modify the data in the `context` property
+-   run `python main.py`
+-   open the `output.docx` file in the output folder
+
+## Deploy to Gcloud
+
+-   install `gcloud` tools https://cloud.google.com/sdk/docs
+-   `gcloud init` and select existing project or create new one
+-   `gcloud functions list` to show the list of functions
+-   `gcloud functions deploy template --runtime python37 --trigger-http --allow-unauthenticated` to deploy the simple test function
+-   `gcloud functions describe template` 
+-   `gcloud functions logs read template`
--- main.py
+++ main.py
@@ -1,89 +1,41 @@
 from docxtpl import DocxTemplate, RichText, InlineImage
-
-doc = DocxTemplate("templates/template.docx")
-
-
-def renderContact(doc, mobile, email):
-    rt = RichText('')
-    rt.add(email, url_id=doc.build_url_id(
-        'mailto:{}'.format(email)), underline=True, color='#3495eb')
-    rt.add('\n{}'.format(mobile))
-    return rt
+from flask import escape, send_file
+from io import open, BytesIO
+import tempfile
+import base64
+import os
 
 
-context = {
-    'author': {
-        'title': 'Solution Architect',
-        'name': 'Anthony Nguyen',
-    },
-    'customer': {
-        'title': 'Product Owner',
-        'name': 'John Charles',
-    },
-    'project': {
-        'name': 'SUPERCOOLA POS',
-    },
-    'title': "SUPERCOOLA POS Specification Review",
-    'parties': [
-        {
-            'name': 'John Charles',
-            'company': 'SUPERCOOLA',
-            'role': 'Product Owner',
-            'contact': renderContact(doc, '+61 433 333 222', 'John@SUPERCOOLA.com')
-        },
-        {
-            'name': 'Anthony Nguyen',
-            'company': 'SSW',
-            'role': 'Project Manager',
-            'contact': renderContact(doc, '+61 433 219 388', 'anthony@ssw.com.au')
-        },
-        {
-            'name': 'Michael Smedley',
-            'company': 'SSW',
-            'role': 'Scrum Master',
-            'contact': renderContact(doc, '+61 433 111 000', 'michael@ssw.com.au')
-        },
-    ],
-    'design': InlineImage(doc, 'design.png'),
-    'ui_areas': [
-        'Login and Sign up page',
-        'Home page',
-        'Report',
-        'Checkout'
-    ],
-    'has_more': True,
-    'outscope': [
-        'Payment gateway',
-        'Support after deployment'
-    ],
-    'total': '230 h',
-    'std': '$33,400',
-    'pre': '$30,400',
-    'tasks': [
-        {
-            'name': 'Login screen',
-            'est': '16 h',
-            'std': '$4,100',
-            'pre': '$3,800',
-            'bg': '42f557',
-            'mvp': 'Y'
-        },
-        {
-            'name': 'Sign up screen',
-            'est': '8 h',
-            'std': '$2,100',
-            'pre': '$1,800',
-            'bg': '42f557',
-            'mvp': 'Y'
-        },
-        {
-            'name': 'Google Sign In',
-            'est': '16 h',
-            'std': '$4,100',
-            'pre': '$3,800',
-        },
-    ]
+def json2docx(request):
+    doc = DocxTemplate("template.docx")
+    request_json = request.get_json(silent=True)
 
-}
-doc.render(context)
-doc.save("output/output.docx")
+    # replace base64 image with temp file
+    tempFiles = replaceBase64Image(doc, request_json)
+
+    doc.render(request_json)
+    file_stream = BytesIO()
+    doc.save(file_stream)
+    file_stream.seek(0)
+
+    # Delete tmp file
+    [os.unlink(f) for f in tempFiles]
+
+    return send_file(file_stream, as_attachment=True, attachment_filename='output.docx')
+
+
+def template(request):
+    file_stream = open("template.docx", "rb")
+    return send_file(file_stream, as_attachment=True, attachment_filename='template.docx')
+
+
+def replaceBase64Image(doc, context):
+    def toInlineImg(key):
+        tmp_img_file = tempfile.NamedTemporaryFile(delete=None, suffix='.jpg')
+        tmp_img_file.write(base64.urlsafe_b64decode(context[key]))
+        tmp_img_file.close()
+
+        context[key] = InlineImage(doc, tmp_img_file.name)
+        return tmp_img_file.name
+
+    return [toInlineImg(k) for k in context.keys() if k.startswith('img_')]
